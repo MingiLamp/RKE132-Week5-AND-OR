e776788e9d70f51cef107d4cdf19a1e963cab7d6
MingiLamp. <olari.reitel@tktk.ee>
2024-12-01 17:06:22 +0200

    add week4 code

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿//Math >= 90; Bio >=90, Chem >=90;
+
+int math, bio, chem;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+bio = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chem = Int32.Parse(Console.ReadLine());
+
+
+if (math >= 90 && bio >= 90 && chem >= 90)
+{
+    Console.WriteLine("Congratulation! You got accepted");
+
+}else
+{
+    Console.WriteLine("Your appl;ication cannot be approved.");
+}
---
+++ OROperator/Program.cs
@@ -0,0 +1,23 @@
+﻿// Math >= 90 OR Chem >= 90
+
+//Math && chem || math && || chem && bio
+
+int math, bio, chem;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your biology result:");
+bio = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result:");
+chem = Int32.Parse(Console.ReadLine());
+
+if((math >= 90 && chem >= 90) || (math >= 90 && bio >= 90) || (chem >= 90 && bio >= 90))
+{
+    Console.WriteLine("Congratulation! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application can't be approved.");
+}
